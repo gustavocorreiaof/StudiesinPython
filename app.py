bff5ee665d10122b1c8dbe5dff70723943d508f0
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2025-04-10 01:23:07 -0300

    C1P6

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print('Hello World')
+print('Taste Express')
+print('1. Register restaurant.')
+print('2. List restaurants.')
+print('3. Activate restaurant.')
+print('4. Exit')
+
+option = input('Choose an option: ')  
+print('You chose the option:', option)  
