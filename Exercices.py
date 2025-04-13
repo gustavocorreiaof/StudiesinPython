a8de240c68b5fe6d84c332d22b222850f2d12cea
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2025-04-13 16:15:37 -0300

    End of first session

---
+++ Exercices.py
@@ -0,0 +1,16 @@
+print('Python in Alura programming school')
+
+name = 'Gustavo';
+age = 25;
+print(f'My name is {name} and im {age} years old' );
+
+print("""
+A
+L
+U
+R
+A""");
+
+pi = 3.14159;
+
+print(f'The pi raunded value is {pi:.2f}')
