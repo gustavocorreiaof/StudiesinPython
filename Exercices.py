41ceda3e67f40e46f506661a265f821ff56c8cbb
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2025-04-13 21:54:59 -0300

    End of second section

--- Exercices.py
+++ Exercices.py
@@ -1,16 +1,36 @@
-print('Python in Alura programming school')
+#--EXCERCICES 1
+# 
+# # print('Python in Alura programming school')
 
-name = 'Gustavo';
-age = 25;
-print(f'My name is {name} and im {age} years old' );
+# name = 'Gustavo';
+# age = 25;
+# print(f'My name is {name} and im {age} years old' );
 
-print("""
-A
-L
-U
-R
-A""");
+# print("""
+# A
+# L
+# U
+# R
+# A""");
 
-pi = 3.14159;
+# pi = 3.14159;
 
-print(f'The pi raunded value is {pi:.2f}')
+# print(f'The pi raunded value is {pi:.2f}')
+
+#--EXCERCICES 2
+
+# number = int(input('Insert a number: '));
+
+# if number % 2 == 0:
+#     print('Even Number!')
+# else:
+#     print('Odd number!')
+
+# age = int(input('Insert your age: '))
+
+# if 0 <= age <= 12:
+#     print('Kid!')
+# elif 13 <= age <=18:
+#     print('Teenager')
+# else:
+#     print('Adult')
--- app.py
+++ app.py
@@ -1,8 +1,22 @@
+import os
+
 print('Taste Express')
 print('1. Register restaurant.')
 print('2. List restaurants.')
 print('3. Activate restaurant.')
 print('4. Exit')
 
-option = input('Choose an option: ')  
-print('You chose the option:', option)  
+option = int(input('Choose an option: ')  )
+
+def finish_app():
+    os.system('cls')
+    print('Exit')
+
+if option == 1:
+    print('Register restaurant')
+elif option == 2:
+    print('List Restaurant')
+elif option == 3:
+    print('Activate Restaurant')
+else:
+    finish_app();
